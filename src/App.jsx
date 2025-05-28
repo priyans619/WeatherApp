db936023651ab06842dba5d3b5db584db01420df
priyans619 <priyansh.2b@gmail.com>
2025-05-28 14:14:36 +0530

    import ErrorMessage

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,26 @@
 import Search from "./components/Search";
 import Weather from "./components/Weather";
 import Forecast from "./components/Forecast/Forecast";
+import ErrorMessage from "./components/ErrorMessage"; 
+import { useWeather } from "./context/WeatherContext"; 
 
 function App() {
+  const { error } = useWeather(); // for error
+
   return (
-    <div className="w-screen min-h-screen bg-gradient-to-br from-sky-900 to-blue-800 text-white px-4 py-6">
-      <p className="text-3xl text-center font-semibold mb-6">
+    <div className="w-full min-h-screen bg-gradient-to-br from-sky-900 to-blue-800 text-white px-4 py-6">
+      <h1 className="text-3xl text-center font-semibold mb-6">
         Welcome to Weather App
-      </p>
+      </h1>
+
       <Search />
-      <div className="flex flex-col lg:flex-row justify-center items-start gap-2 mt-6">
-        <div className="w-full lg:w-2/3">
+      {error && <ErrorMessage message={error} />}
+
+      <div className="flex flex-col lg:flex-row justify-center items-stretch gap-6 mt-6 max-w-7xl mx-auto px-2">
+        <div className="w-full lg:w-1/2">
           <Weather />
         </div>
-        <div className="w-full lg:w-2/3">
+        <div className="w-full lg:w-1/2">
           <Forecast />
         </div>
       </div>
