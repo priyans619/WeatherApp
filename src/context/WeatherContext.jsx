11d5a25a5c3b5557e3d83afb8c2c7563f33d845b
priyans619 <priyansh.2b@gmail.com>
2025-05-28 16:36:10 +0530

    add: png

--- src/context/WeatherContext.jsx
+++ src/context/WeatherContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState, useEffect } from "react";
+import { createContext, useContext, useState, useEffect, useRef } from "react";
 import axios from "axios";
 
 const WeatherContext = createContext();
@@ -9,22 +9,28 @@ export const WeatherProvider = ({ children }) => {
   const [data, setData] = useState({});
   const [forecastData, setForecastData] = useState([]);
   const [error, setError] = useState("");
+  const [currentCity, setCurrentCity] = useState("");
+  const intervalRef = useRef(null); //
 
   const fetchWeather = async (city) => {
     const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=${API_KEY}`;
     const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&APPID=${API_KEY}`;
-    
+
     try {
       const [weatherRes, forecastRes] = await Promise.all([
         axios.get(weatherUrl),
         axios.get(forecastUrl),
       ]);
+
       setData(weatherRes.data);
       setForecastData(
-        forecastRes.data.list.filter((item) => item.dt_txt.includes("12:00:00"))
+        forecastRes.data.list.filter((item) =>
+          item.dt_txt.includes("12:00:00")
+        )
       );
       setError("");
       localStorage.setItem("lastCity", city);
+      setCurrentCity(city); // Update current city afterfetch
     } catch (err) {
       setError("City not found. Please try another location.");
       setData({});
@@ -39,7 +45,19 @@ export const WeatherProvider = ({ children }) => {
     }
   };
 
-  // On first load check localStorage for saved city line 27
+  // For short polling refetching every 30s, currentCity changes
+  useEffect(() => {
+    if (!currentCity) return;
+
+    intervalRef.current && clearInterval(intervalRef.current);
+    intervalRef.current = setInterval(() => {
+      fetchWeather(currentCity);
+    }, 30000);
+
+    return () => clearInterval(intervalRef.current); //for clearing old city
+  }, [currentCity]);
+
+  // Ini fetch: localStorage/fallback
   useEffect(() => {
     const savedCity = localStorage.getItem("lastCity") || "London";
     fetchWeather(savedCity);
