e974b05850d81054ea556b987bedc0822e8b77ca
priyans619 <priyansh.2b@gmail.com>
2025-05-28 18:17:28 +0530

    fix: Background

--- src/components/Forecast/ForecastUI.jsx
+++ src/components/Forecast/ForecastUI.jsx
@@ -1,16 +1,14 @@
-// src/components/Forecast/ForecastUI.jsx
-import React from "react";
 
 const ForecastUI = ({ forecastData }) => {
   return (
     <div className="flex justify-center">
-      <div className=" p-2 rounded-xl text-white w-full max-w-md">
-        <h2 className="text-xl font-semibold mb-4 text-center">5-Day Forecast</h2>
+      <div className=" p-1 rounded-xl text-black w-full max-w-lg mt-5">
+        <h2 className="text-2xl text-white font-sans mb-6 text-center">5-Day Forecast</h2>
         <div className="flex flex-col gap-1">
           {forecastData.map((item, index) => (
             <div
               key={index}
-              className="bg-black px-3 rounded-lg flex items-center justify-between"
+              className="bg-white bg-opacity-25 px-4 flex items-center justify-between"
             >
               <p className="font-medium w-20">
                 {new Date(item.dt_txt).toLocaleDateString("en-US", {
